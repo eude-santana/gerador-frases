29cb4d6248da7b00f47bf4d26d60020b62477942
Professor <eddynewgate4@gmail.com>
2025-11-22 10:36:13 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex flex-col font-serif gap-4 bg-gradient-to-r from-teal-500 to-sky-500 h-screen"></div>
+  );
+}
+
+export default App;
---
+++ src/components/PhraseGenerator.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Phrase from "./Phase";
+
+const PhraseGenerator = () => {
+    const [phrase, setPhrase] = useState({ content: "", author: "" });
+
+    return (
+        <div className="min-h-20 flex flex-col gap-4 items-start">
+            <Phrase text={phrase.content} author={phrase.author}/>
+            <button className="bg-gradient-to-r from-teal-500 to-sky-500 p-2 rounded-lg shadow-md text-white">
+                Nova Frase
+            </button>
+        </div>
+    );
+};
+
+export default PhraseGenerator;
